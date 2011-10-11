8e0fb8724386b2707181740e34cefe8c11375e5a
James Smith <james@floppy.org.uk>
2011-10-11 16:58:32 +0100

    Add open source license details

--- AMEEMaterials/CarbonSummaryForm.Designer.cs
+++ AMEEMaterials/CarbonSummaryForm.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace AMEEMaterials
+﻿// Copyright (C) 2011 AMEE UK Ltd. - http://www.amee.com
+// Released as Open Source Software under the BSD 3-Clause license. See LICENSE.txt for details.
+
+namespace AMEEMaterials
 {
     partial class CarbonSummaryForm
     {
--- AMEEMaterials/MaterialInfoButton.cs
+++ AMEEMaterials/MaterialInfoButton.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (C) 2011 AMEE UK Ltd. - http://www.amee.com
+// Released as Open Source Software under the BSD 3-Clause license. See LICENSE.txt for details.
+
+using System;
 using System.Windows.Forms;
 using System.Drawing;
 using Inventor;
